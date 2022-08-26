6a5a884c3f911bed234007f88c6b494a19341735
wk0w <47082609+wk0w@users.noreply.github.com>
2022-08-26 20:10:30 +0200

    Added `ToCompletedValueTask` extension method.
    Added `ToCompletedTask` equivalent for `ValueTask`, which can be
    used instead of `ValueTask.FromResult`.

---
+++ src/Gybs/Extensions/ValueTaskExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace Gybs.Extensions;
+
+/// <summary>
+/// <see cref="ValueTask{TResult}"/> extensions.
+/// </summary>
+public static class ValueTaskExtensions
+{
+    /// <summary>
+    /// Wraps the object into a value task with <see cref="ValueTask.FromResult{TResult}"/>.
+    /// </summary>
+    /// <typeparam name="TObject">The object type.</typeparam>
+    /// <param name="obj">The object.</param>
+    /// <returns>The completed value task.</returns>
+    public static ValueTask<TObject> ToCompletedValueTask<TObject>(this TObject obj) => ValueTask.FromResult(obj);
+}
